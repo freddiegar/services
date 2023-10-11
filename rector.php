01f84a51244e4493e84327e2aa95eef36086f47f
Freddie Gar <freddie.gar@outlook.com>
2023-10-11 08:34:49 -0500

    Updated Rector: move to 0.18.0

--- rector.php
+++ rector.php
@@ -17,26 +17,30 @@ use Rector\Set\ValueObject\SetList;
 // @see https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
 
 return static function (RectorConfig $configurator): void {
+    $configurator->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/app',
+        __DIR__ . '/tests',
+    ]);
+
     $configurator->import(SetList::CODE_QUALITY);
     $configurator->import(SetList::CODING_STYLE);
     $configurator->import(SetList::DEAD_CODE);
     // $configurator->import(SetList::PHP_74);
     $configurator->import(SetList::PHP_81);
 
-    $parameters = $configurator->parameters();
-
-    $parameters->set(Option::SKIP, [
+    $configurator->skip([
         \Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector::class,
         \Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector::class,
-        \Rector\CodeQuality\Rector\FuncCall\ArrayKeysAndInArrayToArrayKeyExistsRector::class,
+        // \Rector\CodeQuality\Rector\FuncCall\ArrayKeysAndInArrayToArrayKeyExistsRector::class,
         \Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector::class,
         \Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector::class,
         \Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class,
         \Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector::class,
         \Rector\CodeQuality\Rector\Isset_\IssetOnPropertyObjectToPropertyExistsRector::class, // @see https://github.com/rectorphp/rector/issues/6642
         \Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class,
-        \Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector::class,
-        \Rector\CodingStyle\Rector\ClassMethod\RemoveDoubleUnderscoreInMethodNameRector::class,
+        // \Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector::class,
+        // \Rector\CodingStyle\Rector\ClassMethod\RemoveDoubleUnderscoreInMethodNameRector::class,
         \Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector::class,
         \Rector\CodingStyle\Rector\Closure\StaticClosureRector::class,
         \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
@@ -51,7 +55,7 @@ return static function (RectorConfig $configurator): void {
         \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
     ]);
 
-    $services = $configurator->services();
+    // $services = $configurator->singleton(ServiceType::class);
 
     // Translate from @var to scalar value
     // - /**
@@ -59,5 +63,5 @@ return static function (RectorConfig $configurator): void {
     // -  */
     // - public $foo;
     // + public bool $foo;
-    $services->set(TypedPropertyRector::class);
+    // $services->set(TypedPropertyRector::class);
 };
