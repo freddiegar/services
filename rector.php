dae9046cde07d26ef2780902c9e5b497f20185dc
Freddie Gar <freddie.gar@outlook.com>
2024-02-19 17:48:03 -0500

    Updated Rector: move to 1.0.0 (add alias s[y]ntax)

--- rector.php
+++ rector.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 /*
  * This file is part of FlexPHP.
  *
@@ -7,29 +8,18 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-require_once \realpath('/home/freddie/.config/composer/vendor/autoload.php');
+require_once realpath('/home/freddie/.config/composer/vendor/autoload.php');
 
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 
 // @see https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
 
-return static function (RectorConfig $configurator): void {
-    $configurator->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/app',
-        __DIR__ . '/tests',
-    ]);
+return RectorConfig::configure()
+    ->withPhpSets()
+    ->withPreparedSets(deadCode: true, codeQuality: true, codingStyle: true)
 
-    $configurator->import(SetList::CODE_QUALITY);
-    $configurator->import(SetList::CODING_STYLE);
-    $configurator->import(SetList::DEAD_CODE);
-    // $configurator->import(SetList::PHP_74);
-    $configurator->import(SetList::PHP_81);
-
-    $configurator->skip([
+    ->withSkip([
         \Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector::class,
         \Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector::class,
         // \Rector\CodeQuality\Rector\FuncCall\ArrayKeysAndInArrayToArrayKeyExistsRector::class,
@@ -39,29 +29,20 @@ return static function (RectorConfig $configurator): void {
         \Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector::class,
         \Rector\CodeQuality\Rector\Isset_\IssetOnPropertyObjectToPropertyExistsRector::class, // @see https://github.com/rectorphp/rector/issues/6642
         \Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class,
+        \Rector\CodingStyle\Rector\String_\UseClassKeywordForClassNameResolutionRector::class,
         // \Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector::class,
         // \Rector\CodingStyle\Rector\ClassMethod\RemoveDoubleUnderscoreInMethodNameRector::class,
-        \Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector::class,
+        // \Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector::class,
         \Rector\CodingStyle\Rector\Closure\StaticClosureRector::class,
         \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
         \Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector::class,
         \Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector::class,
-        \Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector::class,
+        // \Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector::class,
         \Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector::class,
         \Rector\DeadCode\Rector\ClassMethod\RemoveUnusedConstructorParamRector::class,
+        \Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector::class,
         \Rector\Php55\Rector\String_\StringClassNameToClassConstantRector::class,
         \Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,
         \Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class,
         \Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class,
     ]);
-
-    // $services = $configurator->singleton(ServiceType::class);
-
-    // Translate from @var to scalar value
-    // - /**
-    // -  * @var boolean
-    // -  */
-    // - public $foo;
-    // + public bool $foo;
-    // $services->set(TypedPropertyRector::class);
-};
